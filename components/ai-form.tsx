cd239efbd2f9374fdb514887c556328c2139d3fe
iamyare <yarephotographer@gmail.com>
2024-06-09 10:31:55 -0600

    chore: Add text gradient loading animation and background shine animation

--- components/ai-form.tsx
+++ components/ai-form.tsx
@@ -1,92 +1,162 @@
-"use client"
-
-
 'use client'
 
-import { useMediaQuery } from '@/hooks/use-media-query'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger
-} from '@/components/ui/dialog'
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger
-} from '@/components/ui/drawer'
-
-
-import { Button } from "./ui/button"
-import { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import { useCallback, useEffect, useState, useTransition } from 'react'
 import { Sparkles, Terminal } from 'lucide-react'
 import { Input } from './ui/input'
 
-const title = 'Crear comando'
-const description = 'Elija un nombre para el comando y escriba el código que se ejecutará cuando se invoque el comando.'
+import { AnimatePresence, motion } from 'framer-motion'
+import { gemini } from '@/lib/gemini'
 
 type IAFormInputProps = {
-    field: any
-    watch: any
+  field: any
+  watch: any
 }
 
-export function IAFormInput({field, watch}: IAFormInputProps) {
-    const [open, setOpen] = useState(false)
-    const commandValue = watch("command");
-    useEffect(() => {
-        if (commandValue.startsWith('#')) {
-          console.log('El comando comienza con #:', commandValue);
-          setOpen(true);
-        }
-        // Opcionalmente, manejar el caso de cuando no comienza con '#' si es necesario
-      }, [commandValue]);
+const variants = {
+  hidden: { opacity: 0.5, y: 10 },
+  visible: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 10 }
+}
+
+type CommandAI = {
+  title: string
+  command: string
+}
+
+export function IAFormInput({ field, watch }: IAFormInputProps) {
+  const [open, setOpen] = useState(false)
+  const [isAIPending, setIsAITransition] = useTransition()
+  const [commandAi, setCommandAi] = useState<CommandAI[] | null>(null)
+
+  const commandValue = watch('command')
+  useEffect(() => {
+    if (commandValue.startsWith('#')) {
+      setOpen(true)
+    } else {
+      setOpen(false)
+    }
+  }, [commandValue])
+
+  const promptAI = useCallback(() => {
+    if (!commandValue.startsWith('#')) {
+      return
+    }
+    //Eliminar el #
+    setIsAITransition(async () => {
+      const prompt = commandValue.slice(1)
+      const SO = 'MacOs'
+      const promptStart = `Based on the input "${prompt}", generate 1 to 3 terminal commands for ${SO}. Avoid duplicate responses and additional comments. 
+Example: 
+Input: "open calculator"
+Expected response: [{title: "Open Calculator in MacOs", command: "open -a Calculator.app"}]
+Input: "show list of files in the folder"
+Expected response: [{title: "List files in the folder", command: "ls"}, {title: "List files in the folder with details", command: "ls -l"}]`
+      const response = await gemini.generateContent(promptStart)
+      let commandResponse = response.response.text()
+
+      // Eliminar las comillas adicionales y los backticks
+      commandResponse = commandResponse
+        .replace(/`/g, '')
+        .replace(/json/g, '')
+        .replace(/\\"/g, "'")
+
+      // Convertir la respuesta de string a objeto
+      let commandResponseObject
+
+      try {
+        commandResponseObject = JSON.parse(commandResponse)
+      } catch (error) {
+        commandResponseObject = { title: error, command: null }
+      }
+      setCommandAi(commandResponseObject)
+      console.log(commandResponseObject)
+    })
+  }, [commandValue])
+
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+      //Presionar control enter
+      if (e.key === 'Enter') {
         e.preventDefault()
-        setOpen(!open)
+        promptAI()
       }
     }
 
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [promptAI])
 
+  return (
+    <div className='relative'>
+      <Terminal className='absolute text-muted-foreground size-4 left-3 top-1/2 -translate-y-1/2' />
 
+      <Input placeholder='open calculator.exe' className='pl-8' {...field} />
 
-  function promptAI() {
-    setOpen(true)
-  }
-
-    return (
-
-        <div className='relative'>
-        <Terminal className='absolute text-muted-foreground size-4 left-3 top-1/2 -translate-y-1/2' />
-
-        <Input
-          placeholder='open calculator.exe'
-          className='pl-8'
-          {...field}
-        />
-
+      <AnimatePresence>
         {open && (
-          <div className='absolute bg-red-500 rounded-md h-[100px] p-4 w-full right-0 -top-[100px]'>
-                hola
-                <br />
-                como
-                <br />
-                esta
-          </div>
+          <motion.div
+            className='absolute bg-background/90 z-[100]  rounded-md max-h-[100px] p-4 w-full right-0 bottom-[45px] backdrop-blur-sm'
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            variants={variants}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+          >
+            <motion.div
+              key={commandAi ? 'commandAiTrue' : 'commandAiFalse'} // Cambia la key basada en el estado de commandAi
+              className='flex flex-col'
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10 }} // Agrega una animación de salida
+              transition={{ duration: 0.5 }}
+            >
+              {commandAi ? (
+                <>
+                  {commandAi.map((command, index) => (
+                    <div key={index} className='flex flex-col rounded-md px-2 py-2 hover:bg-accent/50'>
+                      <h4 className='font-medium text-foreground'>
+                        {command.title}
+                      </h4>
+                      <p className='text-muted-foreground text-sm'>
+                        {command.command}
+                      </p>
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <>
+                  {isAIPending ? (
+                    <div className='flex items-center space-x-2'>
+                      <div className='relative'>
+                        <Sparkles className='size-4 text-white' />
+                        <div className='absolute size-2 top-0 right-0 bg-white blur-sm -z-[1] animate-infinite animate-pulse'></div>
+                        <div className='absolute size-5 top-0 right-0 bg-primary blur-md -z-[1] animate-infinite animate-pulse'></div>
+                      </div>
+                      <p className='animate-background-shine text-gradient-loading bg-[length:250%_100%] bg-clip-text text-transparent'>
+                        Generando comando...
+                      </p>
+                    </div>
+                  ) : (
+                    <div className='flex flex-col'>
+                      <h4 className='font-medium text-foreground'>
+                        🤖 AI Command Generator
+                      </h4>
+                      <p className='text-muted-foreground text-sm'>
+                        Press{' '}
+                        <kbd className='pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
+                          ENTER ↵
+                        </kbd>{' '}
+                        to generate a command...
+                      </p>
+                    </div>
+                  )}
+                </>
+              )}
+            </motion.div>
+          </motion.div>
         )}
-      </div>
-      
-    )
-
+      </AnimatePresence>
+    </div>
+  )
 }
-
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "background-shine": {
+      "from": {
+        "backgroundPosition": "0 0"
+      },
+      "to": {
+        "backgroundPosition": "-200% 0"
+      }
+    },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+          "background-shine": "background-shine 2s linear infinite",
       },
       boxShadow: {
         'ring-x2': 'var(--tw-ring-offset-shadow), var(--tw-ring-shadow), 0 0 10px 5px var(--tw-ring-color), var(--tw-shadow, 0 0 #0000)',
