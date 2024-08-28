eb555cf793db267ef6ad6dcf7bf2e5b01d1f1a7a
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-08-28 11:30:28 -0600

    chore: Optimizar componente SuggestionItem y actualizar descripciones

--- app/exc/components/suggestion-item.tsx
+++ app/exc/components/suggestion-item.tsx
@@ -7,7 +7,6 @@ import {
   CardTitle
 } from '@/components/ui/card'
 
-
 export default function SuggestionItem({
   title,
   description,
@@ -17,27 +16,30 @@ export default function SuggestionItem({
   title: string
   description: string
   command: string
-    os: string
+  os: string
 }) {
   return (
     <Card className=' bg-background/20 h-32 w-80 relative overflow-hidden  '>
       <CardContent className='p-4 w-full h-full overflow-y-auto'>
         <CardTitle>{title}</CardTitle>
         <CardDescription className=' flex flex-col space-y-2'>
-          <p>{description}</p>
+          <span>{description}</span>
           <code className='text-xs text-muted-foreground bg-muted p-2 rounded-lg'>
             {command}
           </code>
         </CardDescription>
       </CardContent>
-    <div className='absolute bottom-0 right-0 p-2'>
-      {os === 'win32' && (
-        <Icons.windows className='size-4 fill-foreground/90' />
-      )}
-      {os === 'darwin' && (
-        <Icons.apple className='size-4 fill-foreground/90' />
-      )}
-    </div>
+
+      <div className='absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-background/70 to-transparent pointer-events-none' />
+
+      <div className='absolute bottom-0 right-0 p-2'>
+        {os === 'win32' && (
+          <Icons.windows className='size-4 fill-foreground/90' />
+        )}
+        {os === 'darwin' && (
+          <Icons.apple className='size-4 fill-foreground/90' />
+        )}
+      </div>
     </Card>
   )
 }
--- lib/suggestions.ts
+++ lib/suggestions.ts
@@ -1,38 +1,39 @@
 export const SUGGESTIONS_COMMANDS = [
-    {
-        title: 'Abrir perfil de Instagram',
-        description: 'Abre el perfil de Instagram de un usuario',
-        command: 'open https://www.instagram.com/i.am.yare/',
-        os: 'darwin'
-    },
-    {
-        title: 'Abrir Netflix en el navegador y subir el volumen al 50%',
-        description: 'Abre Netflix y ajusta el volumen del sistema al 50%',
-        command: 'open https://www.netflix.com/ && osascript -e "set volume output volume 50"',
-        os: 'darwin'
-    },
-    {
-        title: 'Abrir aplicación de Spotify',
-        description: 'Abre la aplicación de Spotify en el sistema',
-        command: 'open -a Spotify',
-        os: 'darwin'
-    },
-    {
-        title: 'Silenciar volumen',
-        description: 'Silencia el volumen del sistema',
-        command: 'osascript -e "set volume output muted true"',
-        os: 'darwin'
-    },
-    {
-        title: 'Abrir Explorador de Archivos',
-        description: 'Abre el Explorador de Archivos en Windows',
-        command: 'explorer',
-        os: 'win32'
-    },
-    {
-        title: 'Apagar el sistema',
-        description: 'Apaga la computadora',
-        command: 'shutdown /s',
-        os: 'win32'
-    },
-];
+  {
+    title: 'Abrir perfil de Instagram',
+    description: 'Abre el perfil de Instagram de un usuario',
+    command: 'open https://www.instagram.com/i.am.yare/',
+    os: 'darwin'
+  },
+  {
+    title: 'Abrir Explorador de Archivos',
+    description: 'Abre el Explorador de Archivos en Windows',
+    command: 'explorer',
+    os: 'win32'
+  },
+  {
+    title: 'Abrir Netflix en el navegador y subir el volumen al 50%',
+    description: 'Abre Netflix y ajusta el volumen del sistema al 50%',
+    command:
+      'open https://www.netflix.com/ && osascript -e "set volume output volume 50"',
+    os: 'darwin'
+  },
+  {
+    title: 'Apagar el sistema',
+    description: 'Apaga la computadora',
+    command: 'shutdown /s',
+    os: 'win32'
+  },
+  {
+    title: 'Abrir aplicación de Spotify',
+    description: 'Abre la aplicación de Spotify en el sistema',
+    command: 'open -a Spotify',
+    os: 'darwin'
+  },
+  {
+    title: 'Silenciar volumen',
+    description: 'Silencia el volumen del sistema',
+    command: 'osascript -e "set volume output muted true"',
+    os: 'darwin'
+  }
+]
