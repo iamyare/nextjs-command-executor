154734e69f6acffa8840f7d5fa8fe32c810f7cca
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-08-31 12:19:41 -0600

    chore: Actualizar drawers

--- app/exc/commands/components/delete-command-modal.tsx
+++ app/exc/commands/components/delete-command-modal.tsx
@@ -1,20 +1,18 @@
 'use client'
 
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
-import { Loader, Trash } from "lucide-react"
+import { Loader, Trash } from 'lucide-react'
 
-
-import { cn } from "@/lib/utils"
-import { useMediaQuery } from "@/hooks/use-media-query"
-import { Button } from "@/components/ui/button"
+import { cn } from '@/lib/utils'
+import { useMediaQuery } from '@/hooks/use-media-query'
+import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+  DialogTrigger
+} from '@/components/ui/dialog'
 import {
   Drawer,
   DrawerClose,
@@ -23,49 +21,67 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+  DrawerTrigger
+} from '@/components/ui/drawer'
+import { Input } from '@/components/ui/input'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { useState, useTransition } from "react"
-import { deleteCommandById } from "@/actions"
-import { useToast } from "@/components/ui/use-toast"
-import { useRouter } from "next/navigation"
-
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { useState, useTransition } from 'react'
+import { deleteCommandById } from '@/actions'
+import { useToast } from '@/components/ui/use-toast'
+import { useRouter } from 'next/navigation'
 
 const INFO = {
-  title: "Delete Command",
-  description: "Are you sure you want to delete this command?",
+  title: 'Delete Command',
+  description: 'Are you sure you want to delete this command?'
 }
 
-export function DeleteCommandModal({nameCommand, idCommand}: {nameCommand: string, idCommand: string}) {
+export function DeleteCommandModal({
+  nameCommand,
+  idCommand
+}: {
+  nameCommand: string
+  idCommand: string
+}) {
   const [open, setOpen] = useState(false)
-  const isDesktop = useMediaQuery("(min-width: 768px)")
-
+  const isDesktop = useMediaQuery('(min-width: 768px)')
 
   if (isDesktop) {
     return (
-      <Dialog open={open} onOpenChange={setOpen} >
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-        <Button variant={'ghost'} className="hover:bg-destructive/20 hover:text-red-500 hover:animate-shake">
-        <Trash className="h-4 w-4 mr-2 " />
-        Eliminar Comando
-        </Button>
-
+          <Button
+            variant={'ghost'}
+            className='hover:bg-destructive/20 hover:text-red-500 hover:animate-shake'
+          >
+            <Trash className='h-4 w-4 mr-2 ' />
+            Eliminar Comando
+          </Button>
         </DialogTrigger>
-        <DialogContent classNameGradient="gradient-experience__1_destructive z-[50]" classNameOverlay="bg-background/50"  className="sm:max-w-[425px] border-destructive/30 bg-destructive/5 ">
-        <DialogHeader>
+        <DialogContent
+          classNameGradient='gradient-experience__1_destructive z-[50]'
+          classNameOverlay='bg-background/50'
+          className='sm:max-w-[425px] border-destructive/30 bg-destructive/5 '
+        >
+          <DialogHeader>
             <DialogTitle>{INFO.title}</DialogTitle>
-            <DialogDescription>
-            {INFO.description}
-            </DialogDescription>
+            <DialogDescription>{INFO.description}</DialogDescription>
           </DialogHeader>
-          <DeleteForm nameCommand={nameCommand} idCommand={idCommand} setOpen={setOpen}/>
+          <DeleteForm
+            nameCommand={nameCommand}
+            idCommand={idCommand}
+            setOpen={setOpen}
+          />
         </DialogContent>
       </Dialog>
     )
@@ -74,22 +90,28 @@ export function DeleteCommandModal({nameCommand, idCommand}: {nameCommand: strin
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-      <DropdownMenuItem className=" hover:!bg-destructive/20 hover:!text-red-500  hover:animate-shake" >
-    <Trash className="h-4 w-4 mr-2" />
-        Eliminar Comando
-    </DropdownMenuItem>
+        <Button variant={'ghost'} className=' hover:bg-destructive/20 hover:text-red-500  hover:animate-shake'>
+          <Trash className='h-4 w-4 mr-2' />
+          Eliminar Comando
+        </Button>
       </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
+      <DrawerContent 
+                classNameGradient='gradient-experience__1_destructive z-[50]'
+          classNameOverlay='bg-background/50'
+      >
+        <DrawerHeader className='text-left'>
           <DrawerTitle>{INFO.title}</DrawerTitle>
-          <DrawerDescription>
-          {INFO.description}
-          </DrawerDescription>
+          <DrawerDescription>{INFO.description}</DrawerDescription>
         </DrawerHeader>
-        <DeleteForm nameCommand={nameCommand} idCommand={idCommand} setOpen={setOpen} className="px-4" />
-        <DrawerFooter className="pt-2">
+        <DeleteForm
+          nameCommand={nameCommand}
+          idCommand={idCommand}
+          setOpen={setOpen}
+          className='px-4'
+        />
+        <DrawerFooter className='pt-2'>
           <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
+            <Button variant='outline'>Cancel</Button>
           </DrawerClose>
         </DrawerFooter>
       </DrawerContent>
@@ -97,30 +119,36 @@ export function DeleteCommandModal({nameCommand, idCommand}: {nameCommand: strin
   )
 }
 
-type DeleteFormProps = React.ComponentProps<"form"> & {
-  nameCommand: string;
-  idCommand: string;
-  setOpen: (open: boolean) => void;
-};
+type DeleteFormProps = React.ComponentProps<'form'> & {
+  nameCommand: string
+  idCommand: string
+  setOpen: (open: boolean) => void
+}
 
-function DeleteForm({ className, nameCommand, idCommand, setOpen }: DeleteFormProps) {
-
+function DeleteForm({
+  className,
+  nameCommand,
+  idCommand,
+  setOpen
+}: DeleteFormProps) {
   const [isPeding, startTransition] = useTransition()
   const { toast } = useToast()
   const router = useRouter()
 
   const FormSchema = z.object({
-    confirm: z.string().min(1, {
-      message: 'El nombre del comando es requerido'
-    }).refine(value => value === nameCommand, {
-      message: 'El nombre del comando no coincide'
-    }),
+    confirm: z
+      .string()
+      .min(1, {
+        message: 'El nombre del comando es requerido'
+      })
+      .refine((value) => value === nameCommand, {
+        message: 'El nombre del comando no coincide'
+      }),
     id: z.string().min(1, {
       message: 'El id del comando es requerido'
     })
-  });
+  })
 
-  
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -131,19 +159,19 @@ function DeleteForm({ className, nameCommand, idCommand, setOpen }: DeleteFormPr
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     startTransition(async () => {
-      const {error} = await deleteCommandById({commandId: data.id})
+      const { error } = await deleteCommandById({ commandId: data.id })
       if (error) {
-         toast({
-          variant: "destructive",
-          title: "Uh oh! Something went wrong.",
-          description: "There was a problem with your request.",
+        toast({
+          variant: 'destructive',
+          title: 'Uh oh! Something went wrong.',
+          description: 'There was a problem with your request.'
         })
         return undefined
       }
       toast({
-        variant: "default",
-        title: "Command deleted",
-        description: `The command ${nameCommand} has been deleted.`,
+        variant: 'default',
+        title: 'Command deleted',
+        description: `The command ${nameCommand} has been deleted.`
       })
       router.refresh()
       setOpen(false)
@@ -152,42 +180,46 @@ function DeleteForm({ className, nameCommand, idCommand, setOpen }: DeleteFormPr
 
   return (
     <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn('grid items-start gap-4', className)}
+      >
+        <FormField
+          control={form.control}
+          name='confirm'
+          render={({ field }) => (
+            <FormItem className=' space-y-1'>
+              <FormLabel>
+                Ingrese <span className=' font-bold'>{nameCommand}</span> para
+                confirmar
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className='border-destructive focus-visible:ring-destructive focus-visible:border-none'
+                  placeholder={nameCommand}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-    <form onSubmit={form.handleSubmit(onSubmit)} className={cn("grid items-start gap-4", className)}>
-
-    <FormField
-        control={form.control}
-        name='confirm'
-        render={({ field }) => (
-          <FormItem className=' space-y-1'>
-            <FormLabel>
-            Ingrese <span className=" font-bold">{nameCommand}</span> para confirmar
-            </FormLabel>
-            <FormControl>
-            <Input type="text" className="border-destructive focus-visible:ring-destructive focus-visible:border-none" placeholder={nameCommand} {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-
-      <Button  variant={'destructive'} >
-        {
-          isPeding ? (
+        <Button variant={'destructive'}>
+          {isPeding ? (
             <>
-            <Loader className="h-4 w-4 animate-spin mr-2" />
-            Eliminando...
+              <Loader className='h-4 w-4 animate-spin mr-2' />
+              Eliminando...
             </>
           ) : (
             <>
-            <Trash className="h-4 w-4 mr-2" />
-            Eliminar comando
+              <Trash className='h-4 w-4 mr-2' />
+              Eliminar comando
             </>
-          )
-        }
-      </Button>
-    </form>
+          )}
+        </Button>
+      </form>
     </Form>
-
   )
 }
--- app/exc/components/tabs-conf.tsx
+++ app/exc/components/tabs-conf.tsx
@@ -11,21 +11,22 @@ export default function TabsConf({
   setOpen: (open: boolean) => void
 }) {
   return (
-    <Tabs defaultValue='account' className=' flex w-full h-full gap-2'>
-      <TabsList className=' flex flex-col h-full bg-transparent justify-start items-start'>
-        <TabsTrigger className=' w-full justify-start' value='account'>
+    <Tabs defaultValue='account' className=' flex flex-col md:flex-row w-full h-full gap-2'>
+      <TabsList className=' flex md:flex-col h-full bg-transparent md:justify-start items-start'>
+        <TabsTrigger className=' w-full md:justify-start' value='account'>
           Cuenta
         </TabsTrigger>
-        <TabsTrigger className=' w-full justify-start' value='api'>
+        <TabsTrigger className=' w-full md:justify-start' value='api'>
           API
         </TabsTrigger>
-        <TabsTrigger className=' w-full justify-start' value='sessions'>
+        <TabsTrigger className=' w-full md:justify-start' value='sessions'>
           Sesiones
         </TabsTrigger>
-        <TabsTrigger className=' w-full justify-start' value='delete'>
+        <TabsTrigger className=' w-full md:justify-start' value='delete'>
           Eliminar cuenta
         </TabsTrigger>
       </TabsList>
+      <div className=' w-full px-4'>
       <TabsContent className=' w-full' value='account'>
         <AccountTabs userId={userId} setOpen={setOpen} />
       </TabsContent>
@@ -33,11 +34,17 @@ export default function TabsConf({
         <APITabs userId={userId} setOpen={setOpen} />
       </TabsContent>
       <TabsContent value='sessions'>
-        <div>Sesiones</div>
+        <div className='text-muted-foreground'>
+          Luego se mostraran las sesiones
+        </div>
       </TabsContent>
       <TabsContent value='delete'>
-        <div>Eliminar cuenta</div>
+      <div className='text-muted-foreground'>
+        Luego se mostrara la opcion de eliminar cuenta
+        </div>
       </TabsContent>
+      </div>
+
     </Tabs>
   )
 }
